d40c81b441a496bbc30ee7fb8dd04f0ccb7cf441
Onnonoka <frederic.dymko@gmail.com>
2019-12-02 18:20:10 +0100

    updated

--- DAI - Projet - DYMKO Frederic - MANUEL Anthony/cryptop__config1.js
+++ DAI - Projet - DYMKO Frederic - MANUEL Anthony/cryptop__config1.js
@@ -1,6 +1,6 @@
 const initialModel1 = {
   config: {  //==================================== Configuration principale ===
-    authors : 'par Bruno Jobard et lui-même',  // remplacez par vos noms
+    authors : 'par DYMKO Frédéric et MANUEL Anthony',  // remplacez par vos noms
     debug   : true,       // afficher les traces de debugage ?
     dataMode: 'offline',  // soit 'online' ou 'offline'
     coins: {  //_________________ cryptos dans le portfolio de l'utilisateur ___
